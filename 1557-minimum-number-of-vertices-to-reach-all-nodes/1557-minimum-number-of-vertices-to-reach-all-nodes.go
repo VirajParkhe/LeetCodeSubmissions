f7ee08e6402f1c01abc921621720299bd1fe9e8a
Viraj Parkhe <virajparkhe92@gmail.com>
2022-05-07 15:14:03 +0530

    Time: 455 ms (8.42%), Space: 21.8 MB (10.53%) - LeetHub

---
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.go
@@ -0,0 +1,31 @@
+type edge struct {
+    in int
+    out int
+}
+
+func findSmallestSetOfVertices(n int, edges [][]int) []int {
+    m := map[int]edge{}
+    for i:=0;i<len(edges);i++{
+        out := edges[i][0]
+        in := edges[i][1]
+        if val, ok := m[in]; !ok{
+            m[in] = edge{1,0}
+        }else{
+            val.in +=1
+            m[in]= val
+        }
+        if val, ok := m[out];!ok{
+            m[out] = edge{0,1}
+        }else{
+            val.out +=1
+            m[out] = val
+        }
+    }
+    ret := []int{}
+    for k, v := range m {
+        if v.in == 0 {
+            ret = append(ret, k)
+        }
+    }
+    return ret
+}
