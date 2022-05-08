962c4fe836eb1e9994f3f458e32c45a68c1aa4cd
Viraj Parkhe <virajparkhe92@gmail.com>
2022-05-08 16:15:00 +0530

    Time: 186 ms (60.00%), Space: 10.6 MB (20.00%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.go
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.go
@@ -1,15 +1,12 @@
 
 func getMaximumXor(nums []int, maximumBit int) []int {
     mask := (1<<maximumBit) - 1
-    xors := []int{}
+    l := len(nums)
+    xors := make([]int, l)
     temp := 0
-    for i:=0;i<len(nums);i++{
-        xors = append(xors, temp^nums[i]^mask)
+    for i:=0;i<l;i++{
+        xors[l-i-1] = temp^nums[i]^mask
         temp = temp^nums[i]
     }
-    ret := []int{}
-    for i:=len(nums)-1;i>=0;i--{
-        ret = append(ret, xors[i])
-    }
-    return ret
+    return xors
 }
