eba74bde5fa138bd5a582faf6247f858967047a6
Viraj Parkhe <virajparkhe92@gmail.com>
2022-05-08 14:25:10 +0530

    Time: 83 ms (53.13%), Space: 6.5 MB (78.13%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.go
@@ -0,0 +1,30 @@
+import "strings"
+func minSteps(s string, t string) int {
+    m := map[rune]int{}
+    for _, r := range s {
+        if v, ok := m[r];ok{
+            m[r] = v+1
+        }else{
+            m[r] = 1
+        }
+    }
+    fmt.Println(m)
+    count := 0
+    for _, r := range t {
+        if v, ok := m[r]; ok {
+            m[r] = v -1
+            if v - 1 == 0{
+                delete(m, r)
+            }
+        }else {
+            count++
+        } 
+    }
+    if count == 0 {
+        for _, v := range m {
+            count += v
+        } 
+    }
+    
+    return count
+}
