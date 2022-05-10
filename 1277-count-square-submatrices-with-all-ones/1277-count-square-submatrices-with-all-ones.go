176704098e5a0eb8c6cfba7337479e48fa07a123
Viraj Parkhe <virajparkhe92@gmail.com>
2022-05-10 16:34:03 +0530

    Time: 130 ms (21.05%), Space: 7.5 MB (66.67%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.go
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.go
@@ -3,9 +3,11 @@ func countSquares(matrix [][]int) int {
     if l > len(matrix[0]) {
         l = len(matrix[0])
     }
+    r := len(matrix)
+    c := len(matrix[0])
     for i:=1;i<l;i++{
-        for k:=i;k<len(matrix);k++{
-            for m:=i;m<len(matrix[0]);m++{
+        for k:=i;k<r;k++{
+            for m:=i;m<c;m++{
                 if matrix[k-1][m-1] >=i && matrix[k-1][m] >=i && matrix[k][m-1] >= i && matrix[k][m] != 0{
                     matrix[k][m] = i+1
                 }
@@ -13,8 +15,8 @@ func countSquares(matrix [][]int) int {
         }
     }
     count := 0
-    for i:=0;i<len(matrix);i++{
-        for j:=0;j<len(matrix[0]);j++{
+    for i:=0;i<r;i++{
+        for j:=0;j<c;j++{
             if matrix[i][j] > 0 {
                 count += matrix[i][j]   
             } 
