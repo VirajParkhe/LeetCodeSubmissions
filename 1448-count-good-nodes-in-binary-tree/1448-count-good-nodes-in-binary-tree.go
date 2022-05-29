7dd0ea7e0d6f648df5037fa6cbf42488da179d86
Viraj Parkhe <virajparkhe92@gmail.com>
2022-05-29 15:44:06 +0530

    Time: 85 ms (87.84%), Space: 10.4 MB (77.03%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.go
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.go
@@ -8,20 +8,24 @@
  */
 func goodNodes(root *TreeNode) int {
     ret := 0
-    findGoodNodes(root, nil, root.Val,&ret)
+    max := root.Val
+    findGoodNodes(root, nil, &max,&ret)
     return ret
 }
 
-func findGoodNodes(root, parent *TreeNode,max int, count *int) {
+func findGoodNodes(root, parent *TreeNode, max, count *int) {
     if root == nil {
         return
     }
-    if parent == nil || ( (root.Val >= max) ) {
+    if parent == nil || ( (root.Val >= *max) ) {
         *count +=1
     }
-    if root.Val > max {
-        max = root.Val
+    if root.Val > *max {
+        *max = root.Val
     }
+    temp := *max
     findGoodNodes(root.Left, root, max, count)
+    *max = temp
     findGoodNodes(root.Right, root, max, count)
+    *max=temp
 }
